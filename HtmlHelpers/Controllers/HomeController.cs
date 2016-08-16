461fa16480e712e77a36e2905c0c2309529cc24a
jamesab <james.brookshire@gmail.com>
2016-08-15 20:01:51 -0700

    Initial commit

---
+++ HtmlHelpers/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using HtmlHelpers.RazorHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HtmlHelpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FromApp_CodeFolder()
+        {            
+
+            return View();
+        }
+
+        public ActionResult FromController()
+        {
+            ViewBag.Helper = MyClassHelpers.RegularModal("From the Contorller. Oh yeah thats right!");
+
+            return View();
+        }
+    }
+}
---
+++ HtmlHelpers/RazorHelpers/MyClassHelpers.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HtmlHelpers.RazorHelpers
+{
+    public static class MyClassHelpers
+    {
+        /// <summary>
+        /// Custom Html Helper using Static Methods
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static HtmlString RegularModal(string name)
+        {
+            return new HtmlString("<div class=\"modal fade\" id=\"myModal\" tabindex=\"-1\" role=\"dialog\" data-backdrop=\"static\" data-show=\"true\" data-keyboard=\"false\" aria-labelledby=\"myModalLabel\">" +
+                "<div class=\"modal-dialog\" role=\"document\">" +
+                    "<div class=\"modal-content\">" +
+                        "<div class=\"modal-header\">" +
+                            "<button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>" +
+                            "<div class=\"modal-title text-center\" id=\"myModalLabel\"><i class=\"glyphicon glyphicon-heart\"></i><br /> " + name + "</div>" +
+                        "</div>" +
+                        "<div class=\"modal-body\">" +
+                        name +
+                        "</div>" +
+                        "<div class=\"modal-footer\">" +
+                            "<button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button>" +
+                        "</div>" +
+                    "</div>" +
+                "</div>" +
+            "</div>");
+        }
+
+        /// <summary>
+        /// Extension Method
+        /// </summary>
+        /// <param name="htmlHelper"></param>
+        /// <param name="src"></param>
+        /// <param name="alt"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        public static MvcHtmlString CustomImage(this HtmlHelper htmlHelper, string src, string alt, int width, int height)
+        {
+            var imageTag = new TagBuilder("image");
+            imageTag.MergeAttribute("src", src);
+            imageTag.MergeAttribute("alt", alt);
+            imageTag.MergeAttribute("width", width.ToString());
+            imageTag.MergeAttribute("height", height.ToString());
+
+            return MvcHtmlString.Create(imageTag.ToString(TagRenderMode.SelfClosing));
+        }
+
+    }
+}
